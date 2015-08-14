4c0f6806130bb96ea9e7d5ccab6e43c94c727506
Valentin PRUGNAUD <valentin@sccnorthwest.com>
2015-08-14 14:54:03 -0700

    Update server utilities

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $app->registerRedirectedDomains([
     'www.northwestseatcovers.com' => 'https://www.nwseatcovers.com',
     'northwestseatcovers.com' => 'https://www.nwseatcovers.com',
 ]);
-$app->registerDomainTemplates(["nwbrands.co", "www.nwbrands.co", "www.nwbrands.ca", "nwbrands.ca"]);
+//$app->registerDomainTemplates(["nwbrands.co", "www.nwbrands.co", "www.nwbrands.ca", "nwbrands.ca"]);
 $app->registerTemplates([ "404", "coming-soon", "domain-new", "domain-sale", "soon" ]);
 
 $app->redirectDomains()->showTemplate()->showDomainTemplates()->showDefault();
